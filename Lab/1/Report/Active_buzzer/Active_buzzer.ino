ada9073f765ceb48b73e2538e03a229e090f2f58
Aymericose <55856943+Aymericose@users.noreply.github.com>
2019-10-31 15:52:09 +0100

    Add files via upload

---
+++ Lab/1/Report/Active_buzzer/Active_buzzer.ino
@@ -0,0 +1,15 @@
+/*
+ * Author: Elecrow Engle
+ * Date:7/12/2017
+ * IDE V1.8.2 
+ * Email:engle@elecrow.com
+ * Function:
+ */
+const int buzzer=5; 
+void setup(){
+  pinMode(buzzer,OUTPUT);
+}
+void loop(){
+  digitalWrite(buzzer, HIGH); // produce sound
+}
+
